dc7bd3ce2d79a0a8e142e5143f505a74f64e22cd
hennano <pittyama37@gmail.com>
2020-06-03 15:31:53 +0900

    update BatchFile class

--- src/net/hennabatch/batchlinker/file/BatchFile.java
+++ src/net/hennabatch/batchlinker/file/BatchFile.java
@@ -1,8 +1,12 @@
 package net.hennabatch.batchlinker.file;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.List;
 
 import net.hennabatch.batchlinker.option.Argument;
+import net.hennabatch.batchlinker.option.EnumOption;
 
 public class BatchFile {
 
@@ -12,8 +16,20 @@ public class BatchFile {
 		this.lines = linesIn;
 	}
 
-	public static BatchFile create(BatchPath path, Argument argo) {
+	public static BatchFile create(BatchPath path, Argument arg) {
+		try {
+			return new BatchFile(Files.readAllLines(path.getRealPath(), Charset.forName(arg.getOptionArgs(EnumOption.CHARSET) != null ? arg.getOptionArgs(EnumOption.CHARSET).get(0) : System.getProperty("file.encoding"))));
+		} catch (IOException e) {
+			return null;
+		}
+	}
 
+	public List<String> getLines() {
+		return lines;
+	}
+	
+	public List<BatchPath> getCallPaths(){
+		
 	}
 
 }
