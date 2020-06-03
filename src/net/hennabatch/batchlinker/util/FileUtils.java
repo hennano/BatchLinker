9cc67b52e610b1c28d51cc852e41e3c2331ae18b
hennano <pittyama37@gmail.com>
2020-06-03 15:08:17 +0900

    change FileUtils::getRealBatchPath

--- src/net/hennabatch/batchlinker/util/FileUtils.java
+++ src/net/hennabatch/batchlinker/util/FileUtils.java
@@ -1,6 +1,10 @@
 package net.hennabatch.batchlinker.util;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileUtils {
 
@@ -12,8 +16,19 @@ public class FileUtils {
 
 	public static String removeExtension(Path path) {
 		String fileName = path.getFileName().toString();
-		if(!fileName.contains(".")) return "";
+		if(!fileName.contains(".")) return fileName;
 		return fileName.substring(0, fileName.lastIndexOf('.'));
 	}
 
+	public static Path getRealBatchPath(Path path) throws IOException{
+		if(path.getFileName().toString().contains(".")) {
+			if(extractionExtension(path).equals("cmd") || extractionExtension(path).equals("bat")) {
+				return path.toRealPath();
+			}
+			throw new IOException(path.toString() + "は.cmd/.batファイルではありません");
+		}
+		if(Files.exists(Paths.get(path.toString() + ".cmd"))) return Paths.get(path.toString() + ".cmd").toRealPath();
+		if(Files.exists(Paths.get(path.toString() + ".bat"))) return Paths.get(path.toString() + ".bat").toRealPath();
+		throw new FileNotFoundException(path.toString() + ".cmd/.batは存在しません");
+	}
 }
